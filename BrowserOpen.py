8624d8550b719ebd6874d7f14eb45c4c2c50c078
Suresh <howtovideos.easy@gmail.com>
2017-06-04 16:02:48 +1000

    Update BrowserOpen.py

--- BrowserOpen.py
+++ BrowserOpen.py
@@ -1,41 +1,52 @@
+
 import time
 import webbrowser
+import subprocess
+import threading
 
-valid = 1
-counter = 1
-check = 1
 
-while (check == 1):
-   if (20 == int((time.strftime("%d", time.localtime())))):# Program performs only if date matches 20(or any date given)
+def open():
+   subprocess.call(["c:/Program Files (x86)/Mozilla Firefox/firefox.exe"])
 
-       while (counter == 1):
-           
-           T = int((time.strftime("%H",time.localtime()))) # Reads the current hour time from computer
-           ff_path = webbrowser.get("c:/Program Files (x86)/Mozilla Firefox/firefox.exe %s").open("https://www.youtube.com/watch?v=uHNxEVY9v98")
-           N = T     # Passes the current hour number to N variable
-           valid = 1
-           
-           while (T != 24) and (valid == 1) : 
-               if (N == int((time.strftime("%H",time.localtime())))):# Comparing to check if computer hour time changes
-                   
-                   T = int((time.strftime("%H",time.localtime())))
-                   valid = 1
-                  
-            
-               else:
-                    valid = 0 
-                    counter = 1
-                    T = int((time.strftime("%H",time.localtime())))
+
+def main():
    
+   valid = 1
+   counter = 1
+   check = 1
+   ourprogram = threading.Thread(target = open)
+   ourprogram.start()
 
-   else:
-       pass
-    
+   while check == 1:
+      
+      
+      if (1 == int((time.strftime("%d", time.localtime())))):# Program performs only if date matches 20(or any date given)
+
+          while (counter == 1):
+              
+              T = int((time.strftime("%H",time.localtime()))) # Reads the current hour time from computer
+              ff_path = webbrowser.open("https://www.youtube.com/watch?v=uHNxEVY9v98")
+              N = T     # Passes the current hour number to N variable
+              valid = 1
+              
+              while (T != 24) and (valid == 1) : 
+                  if (N == int((time.strftime("%H",time.localtime())))):# Comparing to check if computer hour time changes
+                      
+                      T = int((time.strftime("%H",time.localtime())))
+                      valid = 1
+                     
+               
+                  else:
+                       valid = 0 
+                       counter = 1
+                       T = int((time.strftime("%H",time.localtime())))
+      
+
+      else:
+         pass
+             
+if (__name__ == "__main__"):
+   main()
+
+
    
-            
-    
-        
-
-       
-            
-    
